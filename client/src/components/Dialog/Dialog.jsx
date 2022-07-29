b3442b1d39fa2d948f95d4b7dc85db1ae350496e
tipsun91 <tipsun911@gmail.com>
2022-07-30 01:16:39 +0300

    Add sitemap with pkg react-router-dom

---
+++ client/src/components/Dialog/Dialog.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Dialog() {
+  return (
+    <div>
+      <h1>Диалог</h1>
+      <p>Список сообщений игроков</p>
+    </div>
+  );
+}
---
+++ client/src/components/Signin/Signin.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Signin() {
+  return (
+    <div>
+      <h1>Авторизация</h1>
+      <p>Форма авторизации</p>
+    </div>
+  );
+}
