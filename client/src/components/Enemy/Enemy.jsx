fd74f3155fc2bccc8eb8b3d5b9db1ea2bd6551c0
mac <mac@MacBook-Pro-mac.local>
2022-07-30 12:01:16 +0300

    width-height

--- client/src/components/Enemy/Enemy.jsx
+++ client/src/components/Enemy/Enemy.jsx
@@ -5,7 +5,11 @@ function Enemy({ enemy }) {
   return (
     <div
       className="enemy"
-      style={{ transform: ` translate(${enemy.x}px, ${enemy.y}px)` }}
+      style={{
+        transform: ` translate(${enemy.x}px, ${enemy.y}px)`,
+        width: `${enemy.w}px`,
+        height: `${enemy.h}px`,
+      }}
     >
       f
     </div>
