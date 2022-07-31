3d94b37d96fe75410df1636dffd8b9ca99f95bd8
Maksim Kharlamov <kharlamovmm@mail.ru>
2022-07-31 18:10:05 +0300

    GameMenu

--- client/src/components/GameMenu/GameMenu.jsx
+++ client/src/components/GameMenu/GameMenu.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import './GameMenu.css';
 
 function GameMenu() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a data-title="Ты готов?" id="jogar-btn">
+              Войти
+            </a>
+          </li>
+          <li>
+            <a data-title="Ты готов?" id="jogar-btn">
+              Зарегистрироваться
+            </a>
+          </li>
+          <li>
+            <a data-title="Ты готов?" id="jogar-btn">
+              Статистика
+            </a>
+          </li>
+          <li>
+            <a data-title="Ты готов?" id="jogar-btn">
+              Выход
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default GameMenu;
