25326048b210e242c1eabb500442ee649b1e4023
Maksim Kharlamov <kharlamovmm@mail.ru>
2022-08-02 12:20:11 +0300

    good

--- client/src/components/GameMenu/GameMenu.jsx
+++ client/src/components/GameMenu/GameMenu.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './GameMenu.css';
 
 function GameMenu() {
+  const user = {
+    name: 'Max',
+    lvl: 80,
+  };
   return (
     <nav className="anim-show-gamemenu">
       <ul>
-        <li>
-          <a href="/sign/in">Войти</a>
-        </li>
-        <li>
-          <a href="/sign/up">Зарегистрироваться</a>
-        </li>
-        <li>
-          <a href="/profile">Статистика</a>
-        </li>
-        <li>
-          <a href="/rating">Рейтинг</a>
-        </li>
-        <li>
-          <a href="/main">Выход</a>
-        </li>
+        {user ? (
+          <>
+            <li>
+              <Link to="/">Начать игру</Link>
+            </li>
+            <li>
+              <Link to="/game">Настройки</Link>
+            </li>
+            <li>
+              <Link to="/profile">Личный кабинет</Link>
+            </li>
+            <li>
+              <Link to="/rating">Рейтинг игроков</Link>
+            </li>
+            <li>
+              <Link to="/logout">Сменить пользователя</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/sign/in">Войти</Link>
+            </li>
+            <li>
+              <Link to="/sign/up">Зарегистрироваться</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
 
 export default function Main() {
@@ -6,7 +7,7 @@ export default function Main() {
     <nav className="anim-show-main">
       <ul>
         <li>
-          <a href="/game">Начать игру</a>
+          <Link to="/game">Start</Link>
         </li>
       </ul>
     </nav>
