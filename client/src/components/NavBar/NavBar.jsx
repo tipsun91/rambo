e755aac250fff819face2f52daaf0770ef36440d
Aleksandr <Aleksandr266@list.ru>
2022-08-02 17:42:56 +0300

    подтягивание изменений

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,21 @@
-// import React from 'react';
-// import './NavBar.css';
+import React from 'react';
+import './NavBar.css';
 // import { Link } from 'react-router-dom';
 
-// export default function NavBar() {
-//   return (
-//     <nav className="navbar">
-//       <div className="lk">
-//         <h2>
-//           Привет,
-//         </h2>
-//       </div>
-//       <ul className="navbar__list">
-//         <li className="li">
-//           <Link to="/cabinet">ЛК</Link>
-//         </li>
-//         <li className="li">
-//           <Link to="/logout">Выйти</Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// }
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="lk">
+        <h2>Привет, Абдулла!</h2>
+      </div>
+      {/* <ul className="navbar__list">
+        <li className="li">
+          <Link to="/cabinet">ЛК</Link>
+        </li>
+        <li className="li">
+          <Link to="/logout">Выйти</Link>
+        </li>
+      </ul> */}
+    </nav>
+  );
+}
--- server/middlewares/routes.js
+++ server/middlewares/routes.js
@@ -1,6 +1,7 @@
 // Импорт маршрутов.
 module.exports = (app) => {
-  app.use('/api/sign',       require('../routes/sign'));  // Вход/Выход & Регистрация
-  app.use('/api/statistics', require('../routes/statistics'));   // Вход/Выход & Регистрация
-  app.use('/',               require('../routes/index')); // React client
+  app.use('/api/sign', require('../routes/sign')); // Вход/Выход & Регистрация
+  app.use('/api/user', require('../routes/user')); // изменения данных юзера
+  app.use('/api/statistics', require('../routes/statistics')); // Вход/Выход & Регистрация
+  app.use('/', require('../routes/index')); // React client
 };
