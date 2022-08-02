74722504278c540a219626a44cc0bc2b03950d88
Aleksandr <Aleksandr266@list.ru>
2022-08-02 16:36:08 +0300

    смена локации

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -15,6 +15,9 @@ import {
   sendStatistic,
   updateWawes,
   updateEnemies,
+  updateBackgroundWawes2,
+  updateBackgroundWawes3,
+  updatePositionPlayer,
 } from '../../store/gameReducer/reducer';
 
 function App() {
@@ -22,7 +25,7 @@ function App() {
   const dispatch = useDispatch();
   const app = useRef();
   const {
-    enemies, bullets, player, game,
+    enemies, bullets, player, game, backgroundPositionLeft,
   } = useSelector((state) => state.game);
   const [passageWawes, setPassageWawes] = useState(1);
   const [countWawes, setCountWawes] = useState(1);
@@ -111,50 +114,96 @@ function App() {
       }
     }
 
-    if (arrowRight) {
+    if (arrowRight && playGame === 'play') {
       pressedButtons.push('d');
     }
-    if (arrowLeft) {
+    if (arrowLeft && playGame === 'play') {
       pressedButtons.push('a');
     }
-    if (arrowUp) {
+    if (arrowUp && playGame === 'play') {
       pressedButtons.push('w');
     }
-    if (arrowDown) {
+    if (arrowDown && playGame === 'play') {
       pressedButtons.push('s');
     }
-    if (bullet) {
+    // логика скорострельности
+    if (bullet && playGame === 'play') {
       if (Date.now() - timeBullet > 300) {
         pressedButtons.push(' ');
         seTimeBullet(Date.now);
       }
     }
+    // логика появления врагов
     if (Date.now() - timeEnemy > 2000) {
       pressedButtons.push('enemy');
       setTimeEnemy(Date.now());
     }
 
-    dispatch(updateFrame({ player: pressedButtons, mouseCord }));
+    // логика завершения игры
     if (player.hp <= 0) {
       setPlayGame('game-over');
     }
 
+    // логика смены волн врагов
     if (playGame === 'play') {
       if (game.countEnemies === 2 && passageWawes === 1) {
+        // меняем стейт для ожидание смены локации
+        setPlayGame('waiting');
+        // увеличеваем волну
         dispatch(updateWawes());
+        // увеличиваем характеристики врагов
         dispatch(updateEnemies());
+        // стейт чтобы предотвартить заход в этот if каждыем 20 млск
         setPassageWawes(2);
       }
       if (game.countEnemies === 4 && passageWawes === 2) {
+        // меняем стейт для ожидание смены локации
+        setPlayGame('waiting');
+        // увеличеваем волну
         dispatch(updateWawes());
+        // увеличиваем характеристики врагов
         dispatch(updateEnemies());
+        // стейт чтобы предотвартить заход в этот if каждыем 20 млск
         setPassageWawes(3);
       }
+      // логика выгрыша
       if (game.countEnemies === 5) {
         setPlayGame('vin');
       }
     }
+    // главный диспатчэ
+    dispatch(updateFrame({ player: pressedButtons, mouseCord }));
 
+    // логика для смены локации при прохождении первой волны
+    if (playGame === 'waiting' && game.countWawes === 2) {
+      // переходт на вторую локацию
+      dispatch(updateBackgroundWawes2());
+      // меняем позицию героя для прохождения в ворота
+      dispatch(updatePositionPlayer());
+      // когда анимация смены локации закончилась меням стейт снова на 'play'
+      if (backgroundPositionLeft === -2600) {
+        setPlayGame('play');
+      }
+    }
+    // логика для смены локации при прохождении первой волны
+    if (playGame === 'waiting' && game.countWawes === 3) {
+      // переходт на третью локацию
+      dispatch(updateBackgroundWawes3());
+      // меняем позицию героя для прохождения в ворота
+      dispatch(updatePositionPlayer());
+      // когда анимация смены локации закончилась меням стейт снова на 'play'
+      if (backgroundPositionLeft === -5600) {
+        setPlayGame('play');
+      }
+    }
+    // перерендриваем компонет каждые 20 млск при смене локации
+    if (playGame === 'waiting') {
+      setTimeout(() => {
+        setTimeoutFlag((prev) => !prev);
+      }, 20);
+    }
+
+    // перерендриваем компонет каждые 20 млск чтобы играть
     if (playGame === 'play') {
       setTimeout(() => {
         setTimeoutFlag((prev) => !prev);
@@ -163,8 +212,11 @@ function App() {
   }, [timeoutFlag]);
 
   useEffect(() => {
+    // логика завершения игры
     if (playGame === 'game-over') {
+      // записываем время проведенное в игре
       const time = (+Date.now() - +startTime) / 1000;
+      // диспатч для сбора статистики за игру
       dispatch(
         sendStatistic({
           countEnemies: game.countEnemies,
@@ -178,40 +230,49 @@ function App() {
   }, [playGame]);
 
   return (
-    <div ref={app} className="App">
-      {playGame === 'play'
+    <div style={{ backgroundPosition: backgroundPositionLeft }} className="app-back">
+      <div ref={app} className="App">
+        {playGame === 'play'
+          && (
+          <div>
+            <GameBar />
+            <Hero />
+            {bullets && bullets.map((el) => <Bullet key={el.id} bullet={el} />)}
+            {enemies && enemies.map((el) => <Enemy key={el.id} enemy={el} />)}
+          </div>
+          )}
+        {playGame === 'game-over'
+          && (
+          <div className="gameOver">
+            <h1>GAME OVER</h1>
+            <Link className="nes-btn is-primary" to="/">
+              Играть еще раз
+            </Link>
+            <Link className="nes-btn is-warning" to="/main">
+              Вернуться в главное меню
+            </Link>
+          </div>
+          )}
+        {playGame === 'vin'
+            && (
+              <div className="gameOver">
+                <h1>VINNER</h1>
+                <Link className="nes-btn is-primary" to="/">
+                  Играть еще раз
+                </Link>
+                <Link className="nes-btn is-warning" to="/main">
+                  Вернуться в главное меню
+                </Link>
+              </div>
+            )}
+        {playGame === 'waiting'
         && (
-        <div>
+        <div className="App">
           <GameBar />
           <Hero />
-          {bullets && bullets.map((el) => <Bullet key={el.id} bullet={el} />)}
-          {enemies && enemies.map((el) => <Enemy key={el.id} enemy={el} />)}
         </div>
         )}
-      {playGame === 'game-over'
-        && (
-        <div className="gameOver">
-          <h1>GAME OVER</h1>
-          <Link className="nes-btn is-primary" to="/">
-            Играть еще раз
-          </Link>
-          <Link className="nes-btn is-warning" to="/main">
-            Вернуться в главное меню
-          </Link>
-        </div>
-        )}
-      {playGame === 'vin'
-          && (
-            <div className="gameOver">
-              <h1>VINNER</h1>
-              <Link className="nes-btn is-primary" to="/">
-                Играть еще раз
-              </Link>
-              <Link className="nes-btn is-warning" to="/main">
-                Вернуться в главное меню
-              </Link>
-            </div>
-          )}
+      </div>
     </div>
   );
 }
